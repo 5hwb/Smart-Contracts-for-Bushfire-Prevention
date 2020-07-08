5be614432f4702cd973f1f0e7492e97b0899a315
Perryanto Hartono <phar2413@uni.sydney.edu.au>
2020-07-09 02:59:16 +1000

    Made contract based on the sensor nodes instead of admin, and added stuff

--- contracts/NetworkFormation.sol
+++ contracts/NetworkFormation.sol
@@ -1,12 +1,31 @@
 pragma solidity ^0.5.16;
-// written for Solidity version 0.4.18 and above that doesnt break functionality
 
-// A smart contract hosted on the admin nodes that forms the clustered network. 
+// A smart contract hosted by each sensor node that forms the clustered network. 
 contract NetworkFormation {
   struct SensorNode {
-    uint energyLevel;
+    uint nodeID;                  // ID of the node
+    address nodeAddress;          // Ethereum address of the node
+    uint energyLevel;             // give it when initialising
+    uint numOfOneHopClusterHeads; // init to 1
+    
+    address parentNode;   // parent (cluster head) of this node
+    address[] childNodes; // children of this node (if cluster head)
   }
   
   uint numOfClusterHeads;
+  
+  SensorNode[] nodes;
+  
+  function addNode(uint id, address addr) public {
+    address[] memory thingo;
+    SensorNode memory node = SensorNode(id, addr, 0, 1, address(this), thingo);
+    nodes.push(node);
+  }
+  
+  function sendJoinRequest() public {
+    // todo
+  }
+  
+  // TODO: implement the GCA algorithm as described in Lee et al. (2011)
   
 }
