53dc2a758761d61120f447302da0e8811646b708
Perryanto Hartono <phar2413@uni.sydney.edu.au>
2020-07-08 22:20:52 +1000

    Added package.json and Truffle config for Ganache

--- truffle-config.js
+++ truffle-config.js
@@ -35,7 +35,16 @@ module.exports = {
    * $ truffle test --network <network-name>
    */
 
-  networks: {
+  networks: {    
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    develop: {
+      port: 8545
+    }
+
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
