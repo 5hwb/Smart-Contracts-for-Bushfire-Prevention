1b91fdc960b3d8333b0cc03cf55827876cfdbb1d
Perryanto Hartono <phar2413@uni.sydney.edu.au>
2020-07-09 01:53:53 +1000

    Add smart contract for forming the wireless sensor network

---
+++ contracts/NetworkFormation.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.16;
+// written for Solidity version 0.4.18 and above that doesnt break functionality
+
+// A smart contract hosted on the admin nodes that forms the clustered network. 
+contract NetworkFormation {
+  struct SensorNode {
+    uint energyLevel;
+  }
+  
+  uint numOfClusterHeads;
+  
+}
