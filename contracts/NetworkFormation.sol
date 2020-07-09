46e4aa52d6ecbc5dd1d8c99bf3235cba867ba3ab
Perryanto Hartono <phar2413@uni.sydney.edu.au>
2020-07-09 23:59:11 +1000

    Improved the smart contract

--- contracts/NetworkFormation.sol
+++ contracts/NetworkFormation.sol
@@ -7,23 +7,80 @@ contract NetworkFormation {
     address nodeAddress;          // Ethereum address of the node
     uint energyLevel;             // give it when initialising
     uint numOfOneHopClusterHeads; // init to 1
+    bool isClusterHead;           // init to false
     
     address parentNode;   // parent (cluster head) of this node
     address[] childNodes; // children of this node (if cluster head)
+    address[] joinRequestNodes; // nodes that have sent join requests to this node
   }
   
-  uint numOfClusterHeads;
+  uint numOfClusterHeads; // N_CH
+  uint numOfJoinRequests; // N_T
   
+  // TODO change this to a mapping + list - this article can help: https://ethereum.stackexchange.com/questions/15337/can-we-get-all-elements-stored-in-a-mapping-in-the-contract
   SensorNode[] nodes;
   
-  function addNode(uint id, address addr) public {
+  // Add a node to the list of all sensor nodes.
+  function addNode(uint id, address addr, uint energyLevel) public {
     address[] memory thingo;
-    SensorNode memory node = SensorNode(id, addr, 0, 1, address(this), thingo);
+    SensorNode memory node = SensorNode(id, addr, energyLevel, 1, false, address(this), thingo);
     nodes.push(node);
   }
   
-  function sendJoinRequest() public {
+  // CLUSTER HEAD ONLY - Send beacon to prospective child nodes
+  function sendBeacon() public {
     // todo
+  }
+  
+  // Send a join request to cluster head.
+  function sendJoinRequest(address clusterHead) public {
+    // todo
+  }
+  
+  // Register the given node as a cluster head.
+  function registerAsClusterHead(address sensorNode) public {
+    // todo
+  }
+  
+  // Elect the next cluster head for this layer.
+  function electClusterHead(address sensorNode) public {
+    // sort the sensor nodes that sent join requests by energy level in descending order
+    // NOTE: this may not compile but the basic logic is good
+    SensorNode[] memory nodesWithJoinRequests;
+    nodesWithJoinRequests.push(/*all the nodes with join requests*/);
+    nodesWithJoinRequests.sort();
+    
+    uint probability = 65; // 65% chance of being elected?
+    
+    // TODO: find out how to calculate floor operation in Solidity
+    numOfClusterHeads = floor((probability / 100) * numOfJoinRequests);
+    
+    // Select the cluster heads
+    uint memory numOfElectedClusterHeads = 0;
+    address[] chosenClusterHeads;
+    for (uint i = 0; i < nodesWithJoinRequests.length; i++) {
+      // If more than 1 cluster head to select: Select N_CH nodes with the highest energy levels as cluster heads
+      if (numOfClusterHeads > 1) {
+
+        // Get only the top N_CH nodes
+        if (numOfElectedClusterHeads < numOfClusterHeads) {
+          chosenClusterHeads.push(nodesWithJoinRequests[i]);
+          numOfClusterHeadsToElect++;
+        }
+      }
+      // If only 1 cluster head: Select the node with the highest energy level as a cluster head.
+      else {
+        chosenClusterHeads.push(nodesWithJoinRequests[i]);
+      }
+    }
+    
+    // Register the cluster heads
+    for (uint i = 0; i < numOfClusterHeads; i++) {
+      registerAsClusterHead(chosenClusterHeads[i]);
+    }
+    
+    // Register the member nodes for this layer
+    // TODO
   }
   
   // TODO: implement the GCA algorithm as described in Lee et al. (2011)
