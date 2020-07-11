93b7403e54cbc25895e1ac3f47a23c170e485411
Perryanto Hartono <phar2413@uni.sydney.edu.au>
2020-07-11 15:36:28 +1000

    Add tests to ensure QuickSort can do its job

--- test/testnetworkformation.js
+++ test/testnetworkformation.js
@@ -3,7 +3,8 @@
 // * https://kalis.me/assert-reverts-solidity-smart-contract-test-truffle/
 // * https://ethereum.stackexchange.com/questions/48627/how-to-catch-revert-error-in-truffle-test-javascript#48629
 
-const Voting = artifacts.require("NetworkFormation");
+const NetworkFormation = artifacts.require("NetworkFormation");
+const QuickSort = artifacts.require("QuickSort");
 const truffleAssert = require('truffle-assertions');
 
 contract("NetworkFormation test", async accounts => {
@@ -20,6 +21,27 @@ contract("NetworkFormation test", async accounts => {
     //let numCandidates = await instance.numCandidates();
     //assert.equal(numVoters.toNumber(), 0);
     
+  });
+  
+  /***********************************************
+   * TEST - Sorting
+   ***********************************************/
+  it("should sort an array", async () => {
+    //let numCandidates = await instance.numCandidates();
+    //assert.equal(numVoters.toNumber(), 0);
+    let sortInstance = await QuickSort.deployed();
+    let thingo = [9, 2, 73, 3, 6, 2, 29];
+    // sort to [2, 2, 3, 6, 9, 29, 73]
+    let sortedThingo = await sortInstance.sort.call(thingo);
+    console.log("sortedThingo = ");
+    console.log(sortedThingo);
+    assert.equal(sortedThingo[0], 2);
+    assert.equal(sortedThingo[1], 2);
+    assert.equal(sortedThingo[2], 3);
+    assert.equal(sortedThingo[3], 6);
+    assert.equal(sortedThingo[4], 9);
+    assert.equal(sortedThingo[5], 29);
+    assert.equal(sortedThingo[6], 73);
   });
   
 
