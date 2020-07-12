c75bdf995368e712b6f0083d7ecedee1f885ca30
Perryanto Hartono <phar2413@uni.sydney.edu.au>
2020-07-13 01:38:27 +1000

    Add more nodes to sort in test case

--- test/TestNetworkFormation.sol
+++ test/TestNetworkFormation.sol
@@ -21,18 +21,31 @@ contract TestNetworkFormation {
    * TEST - Sorting
    ***********************************************/
    // Testing the getSortedNodes() function
+   // note: address is just a placeholder
    function testSortNodes() public {
-    networkFormation.addNode(1, 0xdCad3a6d3569DF655070DEd06cb7A1b2Ccd1D3AF, 89);
-    networkFormation.addNode(2, 0xdCad3a6d3569DF655070DEd06cb7A1b2Ccd1D3AF, 71);
-    networkFormation.addNode(3, 0xdCad3a6d3569DF655070DEd06cb7A1b2Ccd1D3AF, 53);
-    networkFormation.addNode(4, 0xdCad3a6d3569DF655070DEd06cb7A1b2Ccd1D3AF, 62);
+    networkFormation.addNode(1, 0xdcAD3A6D3569DF655070DEd06CB7A1b2CCd1D3a1, 89);
+    networkFormation.addNode(2, 0xdcAD3A6D3569DF655070DEd06CB7A1b2CCd1D3a1, 71);
+    networkFormation.addNode(3, 0xdcAD3A6D3569DF655070DEd06CB7A1b2CCd1D3a1, 53);
+    networkFormation.addNode(4, 0xdcAD3A6D3569DF655070DEd06CB7A1b2CCd1D3a1, 62);
+    networkFormation.addNode(5, 0xdcAD3A6D3569DF655070DEd06CB7A1b2CCd1D3a1, 90);
+    networkFormation.addNode(6, 0xdcAD3A6D3569DF655070DEd06CB7A1b2CCd1D3a1, 75);
+    networkFormation.addNode(7, 0xdcAD3A6D3569DF655070DEd06CB7A1b2CCd1D3a1, 62);
+    networkFormation.addNode(8, 0xdcAD3A6D3569DF655070DEd06CB7A1b2CCd1D3a1, 52);
+    networkFormation.addNode(9, 0xdcAD3A6D3569DF655070DEd06CB7A1b2CCd1D3a1, 95);
+    networkFormation.addNode(10, 0xdcAD3A6D3569DF655070DEd06CB7A1b2CCd1D3a1, 85);
     
-    // sort to [89, 71, 62, 53]
+    // sort to [95, 90, 89, 85, 75, 71, 62, 62, 53, 52]
     Structs.SensorNode[] memory sortedThingo = networkFormation.getSortedNodes();
-    Assert.equal(sortedThingo[0].energyLevel, 89, "Sorting error");
-    Assert.equal(sortedThingo[1].energyLevel, 71, "Sorting error");
-    Assert.equal(sortedThingo[2].energyLevel, 62, "Sorting error");
-    Assert.equal(sortedThingo[3].energyLevel, 53, "Sorting error");
+    Assert.equal(sortedThingo[0].energyLevel, 95, "Sorting error");
+    Assert.equal(sortedThingo[1].energyLevel, 90, "Sorting error");
+    Assert.equal(sortedThingo[2].energyLevel, 89, "Sorting error");
+    Assert.equal(sortedThingo[3].energyLevel, 85, "Sorting error");
+    Assert.equal(sortedThingo[4].energyLevel, 75, "Sorting error");
+    Assert.equal(sortedThingo[5].energyLevel, 71, "Sorting error");
+    Assert.equal(sortedThingo[6].energyLevel, 62, "Sorting error");
+    Assert.equal(sortedThingo[7].energyLevel, 62, "Sorting error");
+    Assert.equal(sortedThingo[8].energyLevel, 53, "Sorting error");
+    Assert.equal(sortedThingo[9].energyLevel, 52, "Sorting error");
   }
 
   
