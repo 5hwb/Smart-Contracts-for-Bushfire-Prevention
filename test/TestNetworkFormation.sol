7de9c2649ca8741b1e6c11e6199f1eaf71b4dabb
Perryanto Hartono <phar2413@uni.sydney.edu.au>
2020-07-12 15:32:41 +1000

    Add test for NetworkFormation contract.

---
+++ test/TestNetworkFormation.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.10;
+pragma experimental ABIEncoderV2;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/NetworkFormation.sol";
+import "../contracts/Structs.sol";
+
+contract TestNetworkFormation {
+/*  
+  // The address of the voting contract to be tested
+  Voting voting = Voting(DeployedAddresses.Voting());
+
+  // The id of the voter that will be used for testing
+  uint expectedVoterId = 8;
+
+  //The expected owner of adopted pet is this contract
+  address expectedAdopter = address(this);
+  
+  // Testing the unadopt() function
+  function testCannotVoteTwice() public {
+    
+    voting.addCandidate("John", "Labor");
+    voting.addCandidate("Tony", "Liberal");
+    
+    voting.vote("yeh", 1);
+    
+    //bool r;
+    //(r, ) = address(this).call(abi.encodePacked(voting.vote("yeh", 2).selector));
+    //Assert.isFalse(r, "If this is true, something is broken!");
+  }
+  
+*/
+
+  // The address of the voting contract to be tested
+  NetworkFormation networkFormation = NetworkFormation(DeployedAddresses.NetworkFormation());
+
+  // Address of this contract
+  address contractAddress = address(this);
+
+
+  /***********************************************
+   * TEST - Sorting
+   ***********************************************/
+   // Testing the getSortedNode() function
+   function testSortNodes() public {
+    networkFormation.addNode(1, 0xdCad3a6d3569DF655070DEd06cb7A1b2Ccd1D3AF, 89);
+    networkFormation.addNode(2, 0xdCad3a6d3569DF655070DEd06cb7A1b2Ccd1D3AF, 71);
+    networkFormation.addNode(3, 0xdCad3a6d3569DF655070DEd06cb7A1b2Ccd1D3AF, 53);
+    networkFormation.addNode(4, 0xdCad3a6d3569DF655070DEd06cb7A1b2Ccd1D3AF, 62);
+    
+    // sort to [53, 62, 71, 89]
+    Structs.SensorNode[] memory sortedThingo = networkFormation.getSortedNode();
+    //console.log("sortedThingo = ");
+    //console.log(sortedThingo);
+    Assert.equal(sortedThingo[0].energyLevel, 53, "Sorting error");
+    Assert.equal(sortedThingo[1].energyLevel, 62, "Sorting error");
+    Assert.equal(sortedThingo[2].energyLevel, 71, "Sorting error");
+    Assert.equal(sortedThingo[3].energyLevel, 89, "Sorting error");
+  }
+
+  
+}
