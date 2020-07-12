a303c2a7b3a4bb79e264285ddba32ed903c88fa7
Perryanto Hartono <phar2413@uni.sydney.edu.au>
2020-07-12 16:14:27 +1000

    Remove old comment and added note on what to do next

--- test/TestNetworkFormation.sol
+++ test/TestNetworkFormation.sol
@@ -8,30 +8,7 @@ import "../contracts/NetworkFormation.sol";
 import "../contracts/Structs.sol";
 
 contract TestNetworkFormation {
-/*  
-  // The address of the voting contract to be tested
-  Voting voting = Voting(DeployedAddresses.Voting());
-
-  // The id of the voter that will be used for testing
-  uint expectedVoterId = 8;
-
-  //The expected owner of adopted pet is this contract
-  address expectedAdopter = address(this);
-  
-  // Testing the unadopt() function
-  function testCannotVoteTwice() public {
-    
-    voting.addCandidate("John", "Labor");
-    voting.addCandidate("Tony", "Liberal");
-    
-    voting.vote("yeh", 1);
-    
-    //bool r;
-    //(r, ) = address(this).call(abi.encodePacked(voting.vote("yeh", 2).selector));
-    //Assert.isFalse(r, "If this is true, something is broken!");
-  }
-  
-*/
+  // TODO: next step - implement the cluster head election algorithm!
 
   // The address of the voting contract to be tested
   NetworkFormation networkFormation = NetworkFormation(DeployedAddresses.NetworkFormation());
