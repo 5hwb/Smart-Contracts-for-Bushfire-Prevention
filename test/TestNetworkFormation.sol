af14d0f89b35045f06da522dcef5ccc9f6bd0017
Perryanto Hartono <phar2413@uni.sydney.edu.au>
2020-07-16 00:40:12 +1000

    Shorten name of contract address var

--- test/TestNetworkFormation.sol
+++ test/TestNetworkFormation.sol
@@ -10,8 +10,8 @@ import "../contracts/Structs.sol";
 contract TestNetworkFormation {
   // TODO: next step - implement the cluster head election algorithm!
 
-  // The address of the voting contract to be tested
-  NetworkFormation networkFormation = NetworkFormation(DeployedAddresses.NetworkFormation());
+  // The address of the contract to be tested
+  NetworkFormation contract = NetworkFormation(DeployedAddresses.NetworkFormation());
 
   // Address of this contract
   address contractAddress = address(this);
@@ -24,19 +24,19 @@ contract TestNetworkFormation {
    // note: address is just a placeholder
   function testSortNodes() public {
     address dummyAddr = 0xdcAD3A6D3569DF655070DEd06CB7A1b2CCd1D3a1;
-    networkFormation.addNode(1, dummyAddr, 89);
-    networkFormation.addNode(2, dummyAddr, 71);
-    networkFormation.addNode(3, dummyAddr, 53);
-    networkFormation.addNode(4, dummyAddr, 62);
-    networkFormation.addNode(5, dummyAddr, 90);
-    networkFormation.addNode(6, dummyAddr, 75);
-    networkFormation.addNode(7, dummyAddr, 62);
-    networkFormation.addNode(8, dummyAddr, 52);
-    networkFormation.addNode(9, dummyAddr, 95);
-    networkFormation.addNode(10, dummyAddr, 85);
+    contract.addNode(1, dummyAddr, 89);
+    contract.addNode(2, dummyAddr, 71);
+    contract.addNode(3, dummyAddr, 53);
+    contract.addNode(4, dummyAddr, 62);
+    contract.addNode(5, dummyAddr, 90);
+    contract.addNode(6, dummyAddr, 75);
+    contract.addNode(7, dummyAddr, 62);
+    contract.addNode(8, dummyAddr, 52);
+    contract.addNode(9, dummyAddr, 95);
+    contract.addNode(10, dummyAddr, 85);
     
     // sort to [95, 90, 89, 85, 75, 71, 62, 62, 53, 52]
-    Structs.SensorNode[] memory sortedThingo = networkFormation.getSortedNodes();
+    Structs.SensorNode[] memory sortedThingo = contract.getSortedNodes();
     Assert.equal(sortedThingo[0].energyLevel, 95, "Sorting error");
     Assert.equal(sortedThingo[1].energyLevel, 90, "Sorting error");
     Assert.equal(sortedThingo[2].energyLevel, 89, "Sorting error");
@@ -55,15 +55,15 @@ contract TestNetworkFormation {
   function testGetNode() public {
     address dummyAddr1 = 0xaaAD3A6d3889dF677070DED06db7A1b2CCD1d3a1;
     address dummyAddr2 = 0xBbAD3a6D4489dfe77070DED06DB7a198CCD1D3A2;
-    networkFormation.addNode(100, dummyAddr1, 50);
-    networkFormation.addNode(101, dummyAddr2, 35);
+    contract.addNode(100, dummyAddr1, 50);
+    contract.addNode(101, dummyAddr2, 35);
 
-    Structs.SensorNode memory node100 = networkFormation.getNode(dummyAddr1);
+    Structs.SensorNode memory node100 = contract.getNode(dummyAddr1);
     Assert.equal(node100.nodeID, 100, "Retrieval error");
     Assert.equal(node100.nodeAddress, dummyAddr1, "Retrieval error");
     Assert.equal(node100.energyLevel, 50, "Retrieval error");
 
-    Structs.SensorNode memory node101 = networkFormation.getNode(dummyAddr2);
+    Structs.SensorNode memory node101 = contract.getNode(dummyAddr2);
     Assert.equal(node101.nodeID, 101, "Retrieval error");
     Assert.equal(node101.nodeAddress, dummyAddr2, "Retrieval error");
     Assert.equal(node101.energyLevel, 35, "Retrieval error");
