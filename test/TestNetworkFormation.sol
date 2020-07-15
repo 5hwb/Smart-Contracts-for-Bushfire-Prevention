777b8eff07932ba391c3ab71cd65111e179f1c47
Perryanto Hartono <phar2413@uni.sydney.edu.au>
2020-07-16 01:08:58 +1000

    Added node registration function test case, fixed error

--- test/TestNetworkFormation.sol
+++ test/TestNetworkFormation.sol
@@ -11,7 +11,7 @@ contract TestNetworkFormation {
   // TODO: next step - implement the cluster head election algorithm!
 
   // The address of the contract to be tested
-  NetworkFormation contract = NetworkFormation(DeployedAddresses.NetworkFormation());
+  NetworkFormation contAddr = NetworkFormation(DeployedAddresses.NetworkFormation());
 
   // Address of this contract
   address contractAddress = address(this);
@@ -24,19 +24,19 @@ contract TestNetworkFormation {
    // note: address is just a placeholder
   function testSortNodes() public {
     address dummyAddr = 0xdcAD3A6D3569DF655070DEd06CB7A1b2CCd1D3a1;
-    contract.addNode(1, dummyAddr, 89);
-    contract.addNode(2, dummyAddr, 71);
-    contract.addNode(3, dummyAddr, 53);
-    contract.addNode(4, dummyAddr, 62);
-    contract.addNode(5, dummyAddr, 90);
-    contract.addNode(6, dummyAddr, 75);
-    contract.addNode(7, dummyAddr, 62);
-    contract.addNode(8, dummyAddr, 52);
-    contract.addNode(9, dummyAddr, 95);
-    contract.addNode(10, dummyAddr, 85);
+    contAddr.addNode(1, dummyAddr, 89);
+    contAddr.addNode(2, dummyAddr, 71);
+    contAddr.addNode(3, dummyAddr, 53);
+    contAddr.addNode(4, dummyAddr, 62);
+    contAddr.addNode(5, dummyAddr, 90);
+    contAddr.addNode(6, dummyAddr, 75);
+    contAddr.addNode(7, dummyAddr, 62);
+    contAddr.addNode(8, dummyAddr, 52);
+    contAddr.addNode(9, dummyAddr, 95);
+    contAddr.addNode(10, dummyAddr, 85);
     
     // sort to [95, 90, 89, 85, 75, 71, 62, 62, 53, 52]
-    Structs.SensorNode[] memory sortedThingo = contract.getSortedNodes();
+    Structs.SensorNode[] memory sortedThingo = contAddr.getSortedNodes();
     Assert.equal(sortedThingo[0].energyLevel, 95, "Sorting error");
     Assert.equal(sortedThingo[1].energyLevel, 90, "Sorting error");
     Assert.equal(sortedThingo[2].energyLevel, 89, "Sorting error");
@@ -52,21 +52,40 @@ contract TestNetworkFormation {
   /***********************************************
    * TEST - Getting existing nodes
    ***********************************************/
-  function testGetNode() public {
-    address dummyAddr1 = 0xaaAD3A6d3889dF677070DED06db7A1b2CCD1d3a1;
-    address dummyAddr2 = 0xBbAD3a6D4489dfe77070DED06DB7a198CCD1D3A2;
-    contract.addNode(100, dummyAddr1, 50);
-    contract.addNode(101, dummyAddr2, 35);
+   address dummyAddr1 = 0xaaAD3A6d3889dF677070DED06db7A1b2CCD1d3a1;
+   address dummyAddr2 = 0xBbAD3a6D4489dfe77070DED06DB7a198CCD1D3A2;
 
-    Structs.SensorNode memory node100 = contract.getNode(dummyAddr1);
+  function testGetNode() public {
+    contAddr.addNode(100, dummyAddr1, 50);
+    contAddr.addNode(101, dummyAddr2, 35);
+
+    Structs.SensorNode memory node100 = contAddr.getNode(dummyAddr1);
     Assert.equal(node100.nodeID, 100, "Retrieval error");
     Assert.equal(node100.nodeAddress, dummyAddr1, "Retrieval error");
     Assert.equal(node100.energyLevel, 50, "Retrieval error");
 
-    Structs.SensorNode memory node101 = contract.getNode(dummyAddr2);
+    Structs.SensorNode memory node101 = contAddr.getNode(dummyAddr2);
     Assert.equal(node101.nodeID, 101, "Retrieval error");
     Assert.equal(node101.nodeAddress, dummyAddr2, "Retrieval error");
     Assert.equal(node101.energyLevel, 35, "Retrieval error");
+  }
+
+  /***********************************************
+   * TEST - Registration of nodes
+   ***********************************************/
+  function testNodeRegistration() public {
+    Assert.equal(contAddr.getNode(dummyAddr1).isClusterHead, false, "Cluster head has issues");
+    Assert.equal(contAddr.getNode(dummyAddr1).isMemberNode, false, "Cluster head has issues");
+    contAddr.registerAsClusterHead(dummyAddr1);
+    Assert.equal(contAddr.getNode(dummyAddr1).isClusterHead, true, "Cluster head registration has issues");
+    Assert.equal(contAddr.getNode(dummyAddr1).isMemberNode, false, "Cluster head registration has issues");
+    
+    Assert.equal(contAddr.getNode(dummyAddr2).isClusterHead, false, "Node has issues");
+    Assert.equal(contAddr.getNode(dummyAddr2).isMemberNode, false, "Node has issues");
+    contAddr.registerAsMemberNode(dummyAddr2);
+    Assert.equal(contAddr.getNode(dummyAddr2).isClusterHead, false, "Member node registration has issues");
+    Assert.equal(contAddr.getNode(dummyAddr2).isMemberNode, true, "Member node registration has issues");
+    
   }
 
   /***********************************************
