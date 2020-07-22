894a7d4e726e69bab9baba4c57955b075a5e7602
Perryanto Hartono <phar2413@uni.sydney.edu.au>
2020-07-23 02:07:57 +1000

    Remove redundant contract deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,8 @@
-var Structs = artifacts.require("Structs");
+//var Structs = artifacts.require("Structs");
 var NetworkFormation = artifacts.require("NetworkFormation");
 var QuickSort = artifacts.require("QuickSort");
 
 module.exports = function(deployer) {
-  deployer.deploy(Structs);
   deployer.deploy(NetworkFormation);
   deployer.deploy(QuickSort);
 };
