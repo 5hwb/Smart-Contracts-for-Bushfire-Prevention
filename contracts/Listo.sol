f9ec4a270935c42086c9ce1ddd9a50a268cf0d53
Perryanto Hartono <phar2413@uni.sydney.edu.au>
2020-08-16 12:58:55 +1000

    Add documentation for IndexedArray

--- contracts/Listo.sol
+++ contracts/Listo.sol
@@ -3,26 +3,53 @@ pragma solidity ^0.6.10;
 pragma experimental ABIEncoderV2;
 
 library DC {
-  struct IndexedArray {
+    /**
+     * @notice A struct for holding array and entry-to-index mappings
+     */
+    struct IndexedArray {
+    // Array of entries
     uint256[] entries;
+    // Number of entries in the IndexedArray
     uint numEntries;
-    mapping (uint256 => uint) entToIndex; // array entries -> array index
+    // Mapping from array entries -> array index
+    mapping (uint256 => uint) entToIndex; 
   }
 
+  /**
+   * @notice Initialise the IndexedArray.
+   * @param arr The IndexedArray to initialise
+   */
   function initIndexedArray(IndexedArray storage arr) public {
     arr.entries.push(0); // Add a dummy value to represent null
     arr.numEntries = 1;
   }
   
+  /**
+   * @notice Get an element at the given index from an IndexedArray.
+   * @param arr The IndexedArray to get the element from
+   * @param idx The index of the entry to get
+   * @return The element at the given index
+   */
   function get(IndexedArray storage arr, uint256 idx) view public returns(uint256) {
     require(idx < arr.entries.length - 1);
     return arr.entries[idx+1];
   }
   
+  /**
+   * @notice Get an element with the given value from an IndexedArray.
+   * @param arr The IndexedArray to get the element from
+   * @param val The value of the entry to get
+   * @return The element with the given value
+   */
   function getWithVal(IndexedArray storage arr, uint256 val) view public returns(uint256) {
     return arr.entries[arr.entToIndex[val]];
   }
   
+  /**
+   * @notice Add an element to an IndexedArray.
+   * @param arr The IndexedArray to add to
+   * @param val The element to add
+   */
   function add(IndexedArray storage arr, uint256 val) public {
     // Cancel the add if an existing entry with the same value is found
     if (getWithVal(arr, val) != 0) 
@@ -34,13 +61,19 @@ library DC {
     arr.numEntries++;
   }
   
+  /**
+   * @notice Check if an IndexedArray contains an element with the given value.
+   * @param arr The IndexedArray to check
+   * @param val The value of the entry to check
+   * @return True if the element exists in the IndexedArray. False otherwise
+   */
   function contains(IndexedArray storage arr, uint256 val) view public returns(bool) {
     return arr.entToIndex[val] != 0;
   }
   
-  function intersection() public {
-    
-  }
+  // function intersection(IndexedArray storage arr1, IndexedArray storage arr2) public returns(IndexedArray memory res) {
+  // 
+  // }
 
 }
 
