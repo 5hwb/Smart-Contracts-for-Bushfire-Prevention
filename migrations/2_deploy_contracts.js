4c9f66cc316250595b08046e39ffa8add0022743
Perryanto Hartono <phar2413@uni.sydney.edu.au>
2020-08-27 10:53:37 +1000

    Add SensorNodeLibrary to interact with DS.Node instances

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,7 @@
 //var Structs = artifacts.require("Structs");
 var NetworkFormation = artifacts.require("NetworkFormation");
 var IA = artifacts.require("IA");
+var SensorNodeLibrary = artifacts.require("SensorNodeLibrary");
 var Listo = artifacts.require("Listo");
 var QuickSort = artifacts.require("QuickSort");
 
@@ -8,6 +9,10 @@ module.exports = function(deployer) {
   deployer.deploy(IA);
   deployer.link(IA, Listo);
   deployer.link(IA, NetworkFormation);
+
+  deployer.deploy(SensorNodeLibrary);
+  deployer.link(SensorNodeLibrary, NetworkFormation);
+
   deployer.deploy(Listo);
   deployer.deploy(NetworkFormation);
   deployer.deploy(QuickSort);
