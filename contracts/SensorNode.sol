a30d95be76b6f3ff0730ad87a7c471eeedf1b59e
Perryanto Hartono <phar2413@uni.sydney.edu.au>
2020-09-06 21:59:39 +1000

    Add underscore to all remaining function arguments in SensorNode

--- contracts/SensorNode.sol
+++ contracts/SensorNode.sol
@@ -13,32 +13,32 @@ library SensorNode {
 
   /**
    * @notice Initialise the given node struct with the given values.
-   * @param daNode The node to modify
+   * @param _daNode The node to modify
    * @param _id Node ID
    * @param _addr Node address
    * @param _energyLevel Node energy level
    */
-  function initNodeStruct(DS.Node storage daNode, uint _id, uint256 _addr, uint _energyLevel) public {
-    daNode.nodeID = _id;
-    daNode.nodeAddress = _addr;
-    daNode.energyLevel = _energyLevel;
-    daNode.networkLevel = 0; // invalid value for now
+  function initNodeStruct(DS.Node storage _daNode, uint _id, uint256 _addr, uint _energyLevel) public {
+    _daNode.nodeID = _id;
+    _daNode.nodeAddress = _addr;
+    _daNode.energyLevel = _energyLevel;
+    _daNode.networkLevel = 0; // invalid value for now
 
     // TODO relocate this to NetworkFormation?
     // Add a dummy 'null' beacon as the 1st element to make null-checking easy
     uint[] memory dummyAddrs = new uint[](1);
     dummyAddrs[0] = 0;
-    daNode.beacons.push(DS.Beacon(false, 0, 0, dummyAddrs));
+    _daNode.beacons.push(DS.Beacon(false, 0, 0, dummyAddrs));
     
     // Add a dummy 'null' reading as the 1st element to make null-checking easy
-    daNode.sensorReadings.push(DS.SensorReading(0, false));
+    _daNode.sensorReadings.push(DS.SensorReading(0, false));
     
     // Mark the node as active
-    daNode.isActive = true;
+    _daNode.isActive = true;
   }
   
   // // test func for calling library function
-  // function getMeFive(DS.Node storage daNode) public view returns(uint) {
+  // function getMeFive(DS.Node storage _daNode) public view returns(uint) {
   //   return SomeLib.getMeFive();
   // }
   
@@ -60,45 +60,45 @@ library SensorNode {
   
   /**
    * @notice Add a beacon to the given node.
-   * @param daNode The node to modify
+   * @param _daNode The node to modify
    */
-  function addBeacon(DS.Node storage daNode, DS.Beacon memory _beacon) public {
-    daNode.beacons.push(_beacon);
-    daNode.numOfBeacons++;
+  function addBeacon(DS.Node storage _daNode, DS.Beacon memory _beacon) public {
+    _daNode.beacons.push(_beacon);
+    _daNode.numOfBeacons++;
   }
   
   /**
    * @notice Get the 1st beacon sent to this node.
-   * @param daNode The node to get the beacon from
+   * @param _daNode The node to get the beacon from
    * @return The 1st beacon struct sent to the given node
    */
-  function getBeacon(DS.Node storage daNode) public view returns(DS.Beacon memory) {
-    if (daNode.beacons.length > 1) {
-      return daNode.beacons[1];
+  function getBeacon(DS.Node storage _daNode) public view returns(DS.Beacon memory) {
+    if (_daNode.beacons.length > 1) {
+      return _daNode.beacons[1];
     }
-    return daNode.beacons[0];
+    return _daNode.beacons[0];
   }
   
   /**
    * @notice Get the n'th beacon sent to this node.
-   * @param daNode The node to get the beacons from
+   * @param _daNode The node to get the beacons from
    * @param index Array index to get the beacon from
    * @return The beacon struct sent to the given node
    */
-  function getBeaconAt(DS.Node storage daNode, uint index) public view returns(DS.Beacon memory) {
-    if (daNode.beacons.length > 1 && index < daNode.beacons.length - 1) {
-      return daNode.beacons[index + 1];
+  function getBeaconAt(DS.Node storage _daNode, uint index) public view returns(DS.Beacon memory) {
+    if (_daNode.beacons.length > 1 && index < _daNode.beacons.length - 1) {
+      return _daNode.beacons[index + 1];
     }
-    return daNode.beacons[0];
+    return _daNode.beacons[0];
   }
   
   /**
    * @notice Get all beacons from this node.
-   * @param daNode The node to modify
+   * @param _daNode The node to modify
    * @return List of all beacons sent to the given node
    */
-  function getBeacons(DS.Node storage daNode) public view returns(DS.Beacon[] memory) {
-    return daNode.beacons;
+  function getBeacons(DS.Node storage _daNode) public view returns(DS.Beacon[] memory) {
+    return _daNode.beacons;
   }
   
   ////////////////////////////////////////
@@ -109,13 +109,13 @@ library SensorNode {
    * @notice Read sensor input.
    * @param _allNodes List of all DS.Node instances
    * @param _addrToNodeIndex Mapping from DS.Node addresses to their index in the array
-   * @param daNode Node to read readings from 
+   * @param _daNode Node to read readings from 
    * @param _sReadings List of sensor readings to input 
    */
   function readSensorInput(
       DS.Node[] storage _allNodes, 
       mapping(uint => uint) storage _addrToNodeIndex, 
-      DS.Node storage daNode, 
+      DS.Node storage _daNode, 
       DS.SensorReading[] memory _sReadings) public {
     
     // Add incoming sensor readings to this node's list of sensor readings
@@ -125,36 +125,36 @@ library SensorNode {
       // Ignore duplicates and null '0' readings
       bool isnotPresent = true;
       
-      for (uint j = 0; j < daNode.sensorReadings.length; j++) {
+      for (uint j = 0; j < _daNode.sensorReadings.length; j++) {
         // If a match is found, ignore it
-        if (_sReadings[i].reading == daNode.sensorReadings[j].reading) {
+        if (_sReadings[i].reading == _daNode.sensorReadings[j].reading) {
           isnotPresent = false;
         }
       }
       
       if (isnotPresent) {
-        daNode.sensorReadings.push(_sReadings[i]);
-        daNode.numOfReadings++;
+        _daNode.sensorReadings.push(_sReadings[i]);
+        _daNode.numOfReadings++;
       }
     }
   
     // Call this again for parent node (intended effect: send the values all the way up to the sink nodes)
-    if (daNode.parentNode != 0) {
-      DS.Node storage parentDsnode = getNode(_allNodes, _addrToNodeIndex, daNode.parentNode);
-      readSensorInput(_allNodes, _addrToNodeIndex, parentDsnode, daNode.sensorReadings);
+    if (_daNode.parentNode != 0) {
+      DS.Node storage parentDsnode = getNode(_allNodes, _addrToNodeIndex, _daNode.parentNode);
+      readSensorInput(_allNodes, _addrToNodeIndex, parentDsnode, _daNode.sensorReadings);
     }
   }
   
   /**
    * @notice Get all sensor readings sent to this node.
-   * @param daNode The node to get the sensor readings from
+   * @param _daNode The node to get the sensor readings from
    * @return The sensor readings sent to the given node
    */
-  function getSensorReadings(DS.Node storage daNode) public view returns(uint256[] memory) {
-    uint256[] memory sensorReadingsUint = new uint256[](daNode.numOfReadings);
+  function getSensorReadings(DS.Node storage _daNode) public view returns(uint256[] memory) {
+    uint256[] memory sensorReadingsUint = new uint256[](_daNode.numOfReadings);
     
-    for (uint i = 0; i < daNode.numOfReadings; i++) {
-      sensorReadingsUint[i] = daNode.sensorReadings[i+1].reading;
+    for (uint i = 0; i < _daNode.numOfReadings; i++) {
+      sensorReadingsUint[i] = _daNode.sensorReadings[i+1].reading;
     }
     
     return sensorReadingsUint;
@@ -166,18 +166,18 @@ library SensorNode {
   
   /**
    * @notice Identify the nodes which can serve as a backup in case the current cluster head fails by going through the list of withinRangeNodes on all received beacons and then saving the results to the list of backup cluster head addresses for that node.
-   * @param daNode The node to identify backup cluster heads for
+   * @param _daNode The node to identify backup cluster heads for
    */
-  function identifyBackupClusterHeads(DS.Node storage daNode) public {
-    uint256[] memory result = daNode.withinRangeNodes;
+  function identifyBackupClusterHeads(DS.Node storage _daNode) public {
+    uint256[] memory result = _daNode.withinRangeNodes;
     
     // Need to have at least 2 beacons (1st one is the 'null' beacon)
-    if (daNode.beacons.length > 1) {
-      for (uint i = 1; i < daNode.beacons.length; i++) {
-        result = IA.inter(result, daNode.beacons[i].withinRangeNodes);
+    if (_daNode.beacons.length > 1) {
+      for (uint i = 1; i < _daNode.beacons.length; i++) {
+        result = IA.inter(result, _daNode.beacons[i].withinRangeNodes);
       }
       
-      daNode.backupCHeads = result;
+      _daNode.backupCHeads = result;
     }
   }
     
@@ -187,86 +187,86 @@ library SensorNode {
   
   /**
    * @notice Set the energy level of this node.
-   * @param daNode The node to modify
+   * @param _daNode The node to modify
    * @param _eLevel New energy level
    */
-  function setEnergyLevel(DS.Node storage daNode, uint _eLevel) public {
-    daNode.energyLevel = _eLevel;
+  function setEnergyLevel(DS.Node storage _daNode, uint _eLevel) public {
+    _daNode.energyLevel = _eLevel;
   }
   
   /**
    * @notice Set the network level of this node.
-   * @param daNode The node to modify
+   * @param _daNode The node to modify
    * @param _nLevel New network level
    */
-  function setNetworkLevel(DS.Node storage daNode, uint _nLevel) public {
-    daNode.networkLevel = _nLevel;
+  function setNetworkLevel(DS.Node storage _daNode, uint _nLevel) public {
+    _daNode.networkLevel = _nLevel;
   }
     
   /**
    * @notice Set the given node as a cluster head.
-   * @param daNode The node to set
+   * @param _daNode The node to set
    */
-  function setAsClusterHead(DS.Node storage daNode) public {
-    //assert(daNode.isMemberNode == false);
-    daNode.isClusterHead = true;
-    daNode.isMemberNode = false;
+  function setAsClusterHead(DS.Node storage _daNode) public {
+    //assert(_daNode.isMemberNode == false);
+    _daNode.isClusterHead = true;
+    _daNode.isMemberNode = false;
   }
   
   /**
    * @notice Set the given node as a member node.
-   * @param daNode The node to set
+   * @param _daNode The node to set
    */
-  function setAsMemberNode(DS.Node storage daNode) public {
-    //assert(daNode.isClusterHead == false);
-    daNode.isClusterHead = false;
-    daNode.isMemberNode = true;
+  function setAsMemberNode(DS.Node storage _daNode) public {
+    //assert(_daNode.isClusterHead == false);
+    _daNode.isClusterHead = false;
+    _daNode.isMemberNode = true;
   }
   
   /**
    * @notice Set the parent node (cluster head) for this node.
-   * @param daNode The node to modify
+   * @param _daNode The node to modify
    * @param _nodeAddr The address of the cluster head that connects to this node
    */
-  function setParentNode(DS.Node storage daNode, uint256 _nodeAddr) public {
-    daNode.parentNode = _nodeAddr;
+  function setParentNode(DS.Node storage _daNode, uint256 _nodeAddr) public {
+    _daNode.parentNode = _nodeAddr;
   }
   
   /**
    * @notice Add the address of a join request node to this node.
-   * @param daNode The node to modify
+   * @param _daNode The node to modify
    * @param _nodeAddr The address of the node that sent a join request to this node
    */
-  function addJoinRequestNode(DS.Node storage daNode, uint256 _nodeAddr) public {
-    daNode.joinRequestNodes.push(_nodeAddr);
-    daNode.numOfJoinRequests++;
+  function addJoinRequestNode(DS.Node storage _daNode, uint256 _nodeAddr) public {
+    _daNode.joinRequestNodes.push(_nodeAddr);
+    _daNode.numOfJoinRequests++;
   }
 
   /**
    * @notice Add the address of a node within range to this node.
-   * @param daNode The node to modify
+   * @param _daNode The node to modify
    * @param _addr The address of the node within the transmission range of this node
    */
-  function addWithinRangeNode(DS.Node storage daNode, uint256 _addr) public {
-    daNode.withinRangeNodes.push(_addr);
+  function addWithinRangeNode(DS.Node storage _daNode, uint256 _addr) public {
+    _daNode.withinRangeNodes.push(_addr);
   }
     
   /**
    * @notice Deactivate the given node.
-   * @param daNode The node to deactivate
+   * @param _daNode The node to deactivate
    */
-  function deactivateNode(DS.Node storage daNode) public {
-    require(daNode.isActive == true);
-    daNode.isActive = false;
+  function deactivateNode(DS.Node storage _daNode) public {
+    require(_daNode.isActive == true);
+    _daNode.isActive = false;
   }
     
   /**
    * @notice Activate the given node.
-   * @param daNode The node to activate
+   * @param _daNode The node to activate
    */
-  function activateNode(DS.Node storage daNode) public {
-    require(daNode.isActive == false);
-    daNode.isActive = true;
+  function activateNode(DS.Node storage _daNode) public {
+    require(_daNode.isActive == false);
+    _daNode.isActive = true;
   }
     
   ////////////////////////////////////////
@@ -275,11 +275,11 @@ library SensorNode {
   
   /**
    * @notice Get the number of child nodes of the given node.
-   * @param daNode The node to check
+   * @param _daNode The node to check
    * @return The number of child nodes
    */
-  function numOfChildNodes(DS.Node storage daNode) public view returns (uint) {
-    return daNode.childNodes.length;
+  function numOfChildNodes(DS.Node storage _daNode) public view returns (uint) {
+    return _daNode.childNodes.length;
   }
   
   ////////////////////////////////////////
@@ -288,11 +288,11 @@ library SensorNode {
   
    /**
     * @notice Get the addresses of all nodes that sent join requests to this node.
-    * @param daNode The node to get the addresses from
+    * @param _daNode The node to get the addresses from
     * @return The addresses of the join request nodes
     */
-  function getJoinRequestNodes(DS.Node storage daNode) public view returns (uint256[] memory) {
-    return daNode.joinRequestNodes;
+  function getJoinRequestNodes(DS.Node storage _daNode) public view returns (uint256[] memory) {
+    return _daNode.joinRequestNodes;
   }
   
   ////////////////////////////////////////
@@ -301,20 +301,20 @@ library SensorNode {
   
   /**
    * @notice Get the addresses of all nodes within range to this node.
-   * @param daNode The node to get the addresses from
+   * @param _daNode The node to get the addresses from
    * @return The addresses of the nodes within range
    */
-  function getWithinRangeNodes(DS.Node storage daNode) public view returns (uint256[] memory) {
-    return daNode.withinRangeNodes;
+  function getWithinRangeNodes(DS.Node storage _daNode) public view returns (uint256[] memory) {
+    return _daNode.withinRangeNodes;
   }
   
   /**
    * @notice Get the number of nodes within range to this node.
-   * @param daNode The node to check
+   * @param _daNode The node to check
    * @return The number of nodes within range
    */
-  function numOfWithinRangeNodes(DS.Node storage daNode) public view returns (uint) {
-    return daNode.withinRangeNodes.length;
+  function numOfWithinRangeNodes(DS.Node storage _daNode) public view returns (uint) {
+    return _daNode.withinRangeNodes.length;
   }
   
   ////////////////////////////////////////
